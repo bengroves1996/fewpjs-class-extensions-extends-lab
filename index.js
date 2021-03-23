e0d5c6850dd1c6b382106ca64f1633f4e790a1d9
bengroves <bengroves15@gmail.com>
2021-03-23 17:37:12 -0400

    commit tree e2d87d69ef189a0a41830e81103a05dbbba1a465

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+// class Pet {
+//     constructor(name, sound) {
+//       this.name = name;
+//       this.sound = sound;
+//     }
+  
+//     speak() {
+//       return `${this.name} says ${this.sound}!`
+//     }
+//   }
+  
+//   class Dog extends Pet {
+//     // inherits constructor from Pet
+//   }
+
+
+class Polygon {
+    constructor(sides){
+        this.sides = sides
+    }
+    get countSides() {
+        return this.sides.length 
+    }
+
+    get perimeter() {
+       return this.sides.reduce((a, b) => a + b, 0)
+    }
+
+}
+
+  class Triangle extends Polygon {
+    get isValid (){
+        let x = this.sides[0]
+        let y = this.sides[1]
+        let z = this.sides[2]
+        if  ((x+z < y ) || (z+y < x ) || (x+y) < z){
+            return  false
+        }
+        return true
+    }
+  }
+
+
+  class Square extends Polygon{
+
+    get isValid(){
+        let a = this.sides[0]
+        let b = this.sides[1]
+        let c = this.sides[2]
+        let d = this.sides[3]
+        if (a === b && a === c && a === d){
+            return true
+        }
+        return false
+    }
+
+    get area(){
+        return this.sides[0] * this.sides[1]
+        }
+  }
